e0326a21b0efff351ae2f58d832607ac87d34c37
hyogooz <hyosik.dev@gmail.com>
2021-03-25 20:18:03 +0900

    fire base crud

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { firestore } from './firebase';
+import { useCallback, useEffect, useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState<object[]>([]);
+
+  const removeHandler = useCallback(() => {
+    firestore.collection('tasks').doc('').delete();
+  }, []);
+  console.log(tasks);
+
+  const createDocument = useCallback(() => {
+    firestore
+      .collection('practice')
+      .add({ todo: { task: 'hi', id: '1' } })
+      .then((res) => {
+        console.log(res);
+        // setTasks((prevTasks) => prevTasks.concat(res))
+      });
+  }, []);
+
+  const fetchData = useCallback(() => {
+    let tasksData: object[] = [];
+
+    firestore
+      .collection('practice')
+      .get()
+      .then((docs) => {
+        docs.forEach((doc) => {
+          console.log(doc.data());
+
+          tasksData.push({ todo: doc.data().todo, id: doc.id });
+        });
+
+        setTasks((prevTasks) => prevTasks.concat(tasksData));
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={createDocument}>버튼</button>
+    </>
   );
 }
 
---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA0EjWi64AR1TpsqNiveY8R7n7pu9sZx3c',
+  authDomain: 'fir-practice-a662a.firebaseapp.com',
+  projectId: 'fir-practice-a662a',
+  storageBucket: 'fir-practice-a662a.appspot.com',
+  messagingSenderId: '65540619833',
+  appId: '1:65540619833:web:fd4da668b2276a3125d7e8',
+  measurementId: 'G-7BEGVJ1G9W',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
+
+export { firestore };
